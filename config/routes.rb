4b5563bd1d6e5d86b369d5afb6d9e83a600b897f
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-06-23 22:57:15 +0000

    [Add]編集機能ビューの記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  get 'lists/:id' =>'lists#show'
   get 'lists/git'
   get '/top' => 'homes#top'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit' , as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
